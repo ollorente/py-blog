d27b8d5080171fba12a84ff7e179b66403b95acb
ollorente <llorente_oscar@hotmail.com>
2017-07-02 02:04:18 -0500

    Subí la carpeta de py-blog

--- py-blog/urls.py
+++ py-blog/urls.py
@@ -1,20 +1,26 @@
 """py-blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+	https://docs.djangoproject.com/en/1.8/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+	1. Add an import:  from my_app import views
+	2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+	1. Add an iport:  from other_app.views import Home
+	2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+	1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', include('apps.home.urls')),
+
+	url(r'^buscar/', include('apps.buscar.urls'), name='buscar'),
+	url(r'^autor/(?P<u>[\w-]+)/$', include('apps.autor.urls'), name='autor'),
+	url(r'^(?P<u>[\w-]+)/$', include('apps.categoria.urls'), name='categoria'),
+	url(r'^(?P<u>[\w-]+)/(?P<v>\d+)/(?P<w>[\w-]+)/$', include('apps.post.urls'), name='post'),
 ]
